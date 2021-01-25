32c67646d98ecc14468ed101c02503c81d4775d0
prodigos <wilghensantos@gmail.com>
2021-01-25 17:19:01 -0500

    initial set up

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+
+    return (
+        <div className="Nav">
+        <h1>This is the nav bar</h1>
+
+        </div>
+    )
+}
+
+export default NavBar;
