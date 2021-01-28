6ca530d58b50224e2c03bf77c60d5bb12900d6e5
prodigos <wilghensantos@gmail.com>
2021-01-27 21:57:14 -0500

    still working on map error

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,15 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Sidebar = () => {
-  const [ingredients, setIngredients] = useState([{}]);
+  const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
     const fetchIngredients = async (url) => {
       try {
         let res = await axios.get(url);
         setIngredients(res.data);
+        debugger
       } catch (error) {
-        setIngredients("");
+        setIngredients([]);
         console.log(error);
       }
     };
@@ -19,20 +20,20 @@ const Sidebar = () => {
     );
   }, []);
 
-  
-  const displayIngredients = 
-      ingredients.map((ingredient) => {
-          console.log(ingredient);
-          return <option key={ingredient.name}>{displayIngredients}</option>;
-        });
-   
-      debugger;
+  const displayIngredients = () => {
+    ingredients.map((ingredient) => {
+      setIngredients(ingredient.name)
+      console.log(ingredient);
+      return <option key={ingredient.name}>{displayIngredients}</option>;
+    });
 
-return (
+  }
+
+  return (
     <div className="sidebar">
       <label>
         By ingredient:
-        <select onChange={displayIngredients}>{ingredients.name}</select>
+        <select onChange={displayIngredients()}>{ingredients.name}</select>
       </label>
     </div>
   );
