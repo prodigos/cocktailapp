4ae341b524720d486c852e15a1daa5cc9a7beacc
prodigos <wilghensantos@gmail.com>
2021-02-01 19:15:03 -0500

    working on sidebar functionality

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,38 +2,41 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const Sidebar = () => {
+  let selectIngredients = [{ name: "Gin" }, { name: "Vodka" }, { name: "Rum" }];
+  
   const [ingredients, setIngredients] = useState([]);
 
-  useEffect(() => {
-    const fetchIngredients = async (url) => {
-      try {
-        let res = await axios.get(url);
-        setIngredients(res.data);
-        debugger
-      } catch (error) {
-        setIngredients([]);
-        console.log(error);
-      }
-    };
-    fetchIngredients(
-      "https://www.thecocktaildb.com/api/json/v2/9973533/search.php?i=vodka"
-    );
-  }, []);
+  const populateSelect = selectIngredients.map((ingredient) => {
+    return <option key={ingredient.name}>{ingredient.name}</option>;
+  });
 
-  const displayIngredients = () => {
-    ingredients.map((ingredient) => {
-      setIngredients(ingredient.name)
-      console.log(ingredient);
-      return <option key={ingredient.name}>{displayIngredients}</option>;
-    });
 
-  }
+  const handleSelect = (e) => {
+    setIngredients(e.target.value);
+    console.log(ingredients);
+    
+  };
+
+  // useEffect(() => {
+  //   const fetchPopularingredients = async (url) => {
+  //     try {
+  //       let res = await axios.get(url);
+  //       setIngredients(res.data.ingredients);
+  //     } catch (error) {
+  //       setIngredients([]);
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchPopularingredients(
+  //     `https://www.thecocktaildb.com/api/json/v2/9973533/search.php?i=${ingredients}`
+  //   );
+  // }, []);
 
   return (
     <div className="sidebar">
       <label>
         By ingredient:
-        <select onChange={displayIngredients()}>{ingredients.name}</select>
+        <select onChange={handleSelect}>{populateSelect}</select>
       </label>
     </div>
   );
